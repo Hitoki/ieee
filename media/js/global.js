49899b77329a91a0b95842b39b55c55e0c1213b0
Corey Cantrell <coreycantrell@noospheremedia.com>
2010-09-07 01:58:24 +0000

    Added extra space above results; changed tag page header

--- media/js/global.js
+++ media/js/global.js
@@ -353,7 +353,7 @@ function resizeLightboxTab() {
     var windowHeight = $(window).height();
     $('.resources-lightbox-content').css('height', windowHeight - 200);
     $('#resource-tabs .nootabs-selected-tab').css('height', windowHeight - 320);
-    $('.nootabs-selected-tab div.group').css('height', windowHeight - 320);
+    $('.nootabs-selected-tab div.group').css('height', windowHeight - 330);
     $('#xplore-results-container div.group').css('height', windowHeight - 350);
 }
 
