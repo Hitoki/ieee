e082b0b6fb2bd96c87c58be41f9b97973f8814d2
James Yoneda <james@rocketmonkeys.com>
2010-12-10 20:46:15 +0000

    Fixed import paths on script.

--- scripts/create_db.py
+++ scripts/create_db.py
@@ -1,7 +1,11 @@
+import sys
+import os
+
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), '..'))
+
 from getopt import getopt
 import settings
 import subprocess
-import sys
 
 def main():
     
