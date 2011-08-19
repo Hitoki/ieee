29f1ea4188f99ee456316190b7f6dcca02f5ee95
Chris Templin <ctemplin@gmail.com>
2011-08-19 04:33:32 +0000

    Fixing nerfed tooltips.

--- views.py
+++ views.py
@@ -1346,9 +1346,13 @@ def tooltip(request, tag_id=None):
             # Normal Tag
             
             if parent_id is not None:
-                if (setting.DEBUG and parent_id == "all"):
-                    parent = Node.objects.get(node_type=Node.objects.getNodesForType(NodeType.ROOT))
-                    tags = Node.objects.get_tags()
+                if parent_id == "all":
+                    if not settings.DEBUG:
+                        (min_resources, max_resources, min_sectors, max_sectors, min_related_tags, max_related_tags, min_societies, max_societies) = (1,1,1,1,1,1,1,1)
+                        (min_score, max_score) = (1,1)
+                    else:     
+                        parent = Node.objects.get(node_type=Node.objects.getNodesForType(NodeType.ROOT))
+                        tags = Node.objects.get_tags()
                 else:
                     parent = Node.objects.get(id=parent_id)
                     tags = parent.child_nodes
@@ -1365,51 +1369,55 @@ def tooltip(request, tag_id=None):
                 (min_score, max_score) = parent.get_combined_sector_ranges(tags)
             
             elif society_id is not None:
-                if settings.DEBUG and society_id == 'all':
-                    # For All Societies, check all tags to get mins/maxes.
-                    from django.db.models import Count, Min, Max
-                    tags = Node.objects.get_tags()
-                    tags = Node.objects.get_extra_info(tags)
-                    
-                    min_resources = None
-                    max_resources = None
-                    min_sectors = None
-                    max_sectors = None
-                    min_related_tags = None
-                    max_related_tags = None
-                    min_societies = None
-                    max_societies = None
-                    min_score = None
-                    max_score = None
-                    for tag1 in tags.values(
-                        'num_resources1',
-                        'num_sectors1',
-                        'num_related_tags1',
-                        'num_societies1',
-                        'score1',
-                    ):
-                        if min_resources is None or tag1['num_resources1'] < min_resources:
-                            min_resources = tag1['num_resources1']
-                        if max_resources is None or tag1['num_resources1'] > max_resources:
-                            max_resources = tag1['num_resources1']
-                        if min_sectors is None or tag1['num_sectors1'] < min_sectors:
-                            print "new min"
-                            min_sectors = tag1['num_sectors1']
-                        if max_sectors is None or tag1['num_sectors1'] > max_sectors:
-                            print "new max"
-                            max_sectors = tag1['num_sectors1']
-                        if min_related_tags is None or tag1['num_related_tags1'] > min_related_tags:
-                            min_related_tags = tag1['num_related_tags1']
-                        if max_related_tags is None or tag1['num_related_tags1'] < max_related_tags:
-                            max_related_tags = tag1['num_related_tags1']
-                        if min_societies is None or tag1['num_societies1'] > min_societies:
-                            min_societies = tag1['num_societies1']
-                        if max_societies is None or tag1['num_societies1'] < max_societies:
-                            max_societies = tag1['num_societies1']
-                        if min_score is None or tag1['score1'] > min_score:
-                            min_score = tag1['score1']
-                        if max_score is None or tag1['score1'] < max_score:
-                            max_score = tag1['score1']
+                if society_id == 'all':
+                    if not settings.DEBUG:
+                        (min_resources, max_resources, min_sectors, max_sectors, min_related_tags, max_related_tags, min_societies, max_societies) = (1,1,1,1,1,1,1,1)
+                        (min_score, max_score) = (1,1)
+                    else:
+                        # For All Societies, check all tags to get mins/maxes.
+                        from django.db.models import Count, Min, Max
+                        tags = Node.objects.get_tags()
+                        tags = Node.objects.get_extra_info(tags)
+                        
+                        min_resources = None
+                        max_resources = None
+                        min_sectors = None
+                        max_sectors = None
+                        min_related_tags = None
+                        max_related_tags = None
+                        min_societies = None
+                        max_societies = None
+                        min_score = None
+                        max_score = None
+                        for tag1 in tags.values(
+                            'num_resources1',
+                            'num_sectors1',
+                            'num_related_tags1',
+                            'num_societies1',
+                            'score1',
+                            ):
+                            if min_resources is None or tag1['num_resources1'] < min_resources:
+                                min_resources = tag1['num_resources1']
+                            if max_resources is None or tag1['num_resources1'] > max_resources:
+                                max_resources = tag1['num_resources1']
+                            if min_sectors is None or tag1['num_sectors1'] < min_sectors:
+                                print "new min"
+                                min_sectors = tag1['num_sectors1']
+                            if max_sectors is None or tag1['num_sectors1'] > max_sectors:
+                                print "new max"
+                                max_sectors = tag1['num_sectors1']
+                            if min_related_tags is None or tag1['num_related_tags1'] > min_related_tags:
+                                min_related_tags = tag1['num_related_tags1']
+                            if max_related_tags is None or tag1['num_related_tags1'] < max_related_tags:
+                                max_related_tags = tag1['num_related_tags1']
+                            if min_societies is None or tag1['num_societies1'] > min_societies:
+                                min_societies = tag1['num_societies1']
+                            if max_societies is None or tag1['num_societies1'] < max_societies:
+                                max_societies = tag1['num_societies1']
+                            if min_score is None or tag1['score1'] > min_score:
+                                min_score = tag1['score1']
+                            if max_score is None or tag1['score1'] < max_score:
+                                max_score = tag1['score1']
                 else:
                     society = Society.objects.get(id=society_id)
                     (min_resources, max_resources, min_sectors, max_sectors, min_related_tags, max_related_tags, min_societies, max_societies) = society.get_tag_ranges()
