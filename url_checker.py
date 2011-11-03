7507bd478e98c58f59a4bbb5b02fcb8a2fda3062
Chris Templin <ctemplin@gmail.com>
2011-11-03 13:28:33 +0000

    Handling attribute error.

--- url_checker.py
+++ url_checker.py
@@ -214,8 +214,12 @@ def check_url_thread(resource_queue, resources_to_save_queue, checking_hosts):
         # Check if the host is being checked right now... wait until it's not
         
         urlparts = urlparse.urlparse(resource.url)
-        hostname = urlparts.netloc
-        
+        try:
+            hostname = urlparts.netloc
+        except AttributeError e:
+            print 'Got AttributeError, %s' % e
+            continue
+
         #print '  %s:   adding %s to checking_hosts' % (thread.getName(), hostname)
         checking_hosts.block_and_append(hostname)
         
