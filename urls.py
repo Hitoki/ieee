366752f0dafb5215e09c78db14bb9f1375e6d7c9
Chris Templin <ctemplin@gmail.com>
2012-02-16 15:59:23 +0000

    Adding optional trailing slashes to tag and cluster landing pages.

--- urls.py
+++ urls.py
@@ -66,9 +66,9 @@ else:
         url(r'^tags/?$', views.tags_list, name='tags_list'),
         url(r'^tags/starts/(?P<starts_with>.+)/?$', views.tags_starts, name='tags_starts'),
         url(r'^tags/all/?$', views.tags_all,name='tags_all'),
-        url(r'^tag/(?P<tag_id>\d+)/[0-9a-zA-Z_-]*$', views.tag_landing, name='tag_landing'),
+        url(r'^tag/(?P<tag_id>\d+)/[0-9a-zA-Z_-]*/?$', views.tag_landing, name='tag_landing'),
         url(r'^clusters/', views.clusters_list, name='clusters_list'),
-        url(r'^cluster/(?P<cluster_id>\d+)/[0-9a-zA-Z_-]*$', views.cluster_landing, name='cluster_landing'),
+        url(r'^cluster/(?P<cluster_id>\d+)/[0-9a-zA-Z_-]*/?$', views.cluster_landing, name='cluster_landing'),
         
         # Single static SWF file, for Flash player bug
         url(r'^admin/(?P<path>SkinUnderAllNoCaption.swf)$', 'django.views.static.serve', {'document_root': '%s/flash' % settings.MEDIA_ROOT}),
