9a07bff0bd919f5d4c9c8cae2441961fd6e57d36
Corey Cantrell <coreycantrell@noospheremedia.com>
2012-06-25 12:16:11 +0000

    Trying different variable names,

--- management/commands/send_resource_addition_notifications.py
+++ management/commands/send_resource_addition_notifications.py
@@ -36,8 +36,8 @@ class Command(NoArgsCommand):
 
             context = Context({
                 "notification_requests": reqs_with_new_resources,
-                "topic_name": req.node.name,
-                "resource_name": nr.resource.name
+                "topic_name": reqs.name,
+                "resource_name": new_resources.name
             })
             body = loader.get_template('email/notify_email.html').render(context)
     
