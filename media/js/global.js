3f46240f8071124adb4121c175f56fcf4f8924ec
Corey Cantrell <coreycantrell@noospheremedia.com>
2012-09-20 19:55:32 +0000

    Removing extra top bar on education tab.

--- media/js/global.js
+++ media/js/global.js
@@ -332,7 +332,7 @@ function resizeLightboxTab() {
     $('.nootabs-selected-tab div.group').css('height', windowHeight - 310);
     $('#xplore-results-container div.group').css('height', windowHeight - 362);
     $('#job-results-container div.group').css('height', windowHeight - 362);
-    $('#education-results-container div.group').css('height', windowHeight - 362);
+    $('#education-results-container div.group').css('height', windowHeight - 310);
     $('#patents-tab div.group').css('height', windowHeight - 320);
 }
 
