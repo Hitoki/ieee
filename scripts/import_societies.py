7d8bd2ae429c6d6d28bc4f25be9a4c0bebfc88e9
Matt Mikula <mattmikula@noospheremedia.com>
2012-11-08 00:46:14 +0000

    update to script that creates societies from spreadsheet

--- scripts/import_societies.py
+++ scripts/import_societies.py
@@ -1,5 +1,8 @@
+from sys import argv
 import csv
 import MySQLdb
+
+script, filepath, society_name = argv
 
 host = ''
 user = ''
@@ -12,22 +15,16 @@ mydb = MySQLdb.connect (host = host,
                  db = db)
 
 cursor = mydb.cursor()
-filepath = '/home/ubuntu/Downloads/IEEE Initiative Clusters 10-18-2012 - TE.csv'
 reader = csv.reader(open(filepath, "rb"))
 
-society_name = ''
+cursor.execute('INSERT INTO ieeetags_society(name) VALUES(%s)', society_name)
 
-cursor.execute('INSERT INTO ieeetags_society(name)' \
-	          'VALUES("%s")', society_name)
-
-society_id = ''
+society_id = cursor.lastrowid
 
 for row in reader:
-	if row[2] == 1:
-		cursor.execute('INSERT INTO ieeetags_node_society(node_id, \
-	          society_id)' \
-	          'VALUES("%s", "%s")', 
-	    row[0], society_id)
+	if row[2] == '1':
+		cursor.execute('INSERT INTO ieeetags_node_societies(node_id, society_id) VALUES(%s, %s)', 
+	    (row[0], society_id))
 
 mydb.commit()
 cursor.close()    
