128aaa0f47ed8cb75147206839bafc60c9c4e945
Matt Mikula <mattmikula@noospheremedia.com>
2012-12-30 15:55:15 +0000

    replace 'society' with 'organization'

--- site_admin/forms.py
+++ site_admin/forms.py
@@ -91,7 +91,7 @@ class EditTagForm(Form):
     name = CharField(max_length=100, label='Topic Name')
     parents = ModelMultipleChoiceField(queryset=Node.objects.getSectors(), label='Sectors', widget=CheckboxSelectMultipleColumns(columns=3))
     definition = CharField(widget=Textarea(), required=False)
-    societies = MultiSearchField(model=Society, search_url='/admin/ajax/search_societies', label='Organization', widget=MultiSearchWidget(remove_link_flyover_text='Remove Organization from Topic'))
+    societies = MultiSearchField(model=Society, search_url='/admin/ajax/search_societies', label='Organization', widget_label='Associate organizations',widget=MultiSearchWidget(remove_link_flyover_text='Remove Organization from Topic',blur_text='Type a few characters to bring up matching organizations'))
     filters = ModelMultipleChoiceField(queryset=Filter.objects.all(), widget=CheckboxSelectMultipleColumns(columns=2), required=False, label='Filters')
     related_tags = MultiSearchField(model=Node, search_url='/admin/ajax/search_tags', label='Related Topics', widget_label='Associate Related Topics with this Topic',show_create_tag_link=True, widget=MultiSearchWidget(remove_link_flyover_text='Remove Tag from Tag', blur_text='Type a few characters to bring up matching topics'))
 
