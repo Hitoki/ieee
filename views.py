fa855ac5d9cd8da8178794fb408599929c58d568
coreycantrell <ccantrell@lockify.com>
2013-05-09 11:39:57 -0400

    Revert "remove newui flags"
    This reverts commit 1250b953512496473d740e1856c21c548cf49eeb, reversing
    changes made to be4a47986b8317e7f876c84bf109574f0db7b461.

--- views.py
+++ views.py
@@ -1952,6 +1952,7 @@ def ajax_video(request):
 def ajax_welcome(request):
     'Returns the HTML content for the welcome lightbox.'
     return render(request, 'ajax_welcome.html')
+    
 
 def ajax_profile_log(request):
     url = request.REQUEST['url']
