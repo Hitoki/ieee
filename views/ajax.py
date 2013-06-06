00e54151d4f4ee3f6303ccc90d4cbbea23d61c9e
mattmikula <matthewmikula@gmail.com>
2013-06-05 22:57:00 -0400

    remove duplicate functions

--- views/ajax.py
+++ views/ajax.py
@@ -24,6 +24,7 @@ import util
 from BeautifulSoup import BeautifulSoup
 
 from .views import render
+from .xplore import _get_xplore_results
 
 TOOLTIP_MAX_CHARS = 120
 
@@ -199,19 +200,6 @@ def feedback(request):
 def browser_warning(request):
     'Shows the AJAX browser compatability warning page.  Allows the user to click through if they still want to browse the site.'
     return render(request, 'browser_warning.html')
-
-@login_required
-def xplore_full_results(request, tag_id):
-    'Returns full listing of IEEE xplore results for the given tag.'
-    tag = Node.objects.get(id=tag_id)
-
-    results, errors, total_results = _get_xplore_results(tag.name, show_all=True)
-    return render(request, 'xplore_full_results.html', {
-        'tag':tag,
-        'xplore_error': errors,
-        'xplore_results': results,
-        'totalfound': total_results,
-    })
 
 import hotshot
 import os
@@ -469,173 +457,6 @@ XPLORE_SORTS = [
     #XPLORE_SORT_ARTICLE_NUMBER,
     XPLORE_SORT_PUBLICATION_YEAR,
 ]
-
-def _get_xplore_results(tag_name, highlight_search_term=True, show_all=False, offset=0, sort=None, sort_desc=False, ctype=None, recent=False):
-    '''
-    Get xplore results for the given tag_name from the IEEE Xplore search gateway.  Searches all fields for the tag_name phrase, returns results.
-    @return: a 3-tuple of (results, errors, total_results).  'errors' is a string of any errors that occurred, or None.  'total_results' is the total number of results (regardless of how many are returned in 'results'.  'results' is an array of dicts:
-        [
-            {
-                'name': ...
-                'description': ...
-                'url': ...
-            },
-            ...
-        ]
-        
-    '''
-    
-    if show_all:
-        # Some arbitrarily big number...
-        max_num_results = 10000
-    elif recent:
-        max_num_results = 1
-    else:
-        max_num_results = 10
-    
-    if sort is not None and sort not in XPLORE_SORTS:
-        raise Exception('Unknown sort %r' % sort)
-    
-    params = {
-        # Number of results
-        'hc': max_num_results,
-        # Specifies the result # to start from
-        'rs': offset+1
-    }
-
-    if sort:
-        params['sortfield'] = sort
-    if sort_desc:
-        params['sortorder'] = 'desc'
-    if ctype:
-        params['ctype'] = ctype
-        
-    def getElementByTagName(node, tag_name):
-        nodes = node.getElementsByTagName(tag_name)
-        if len(nodes) == 0:
-            return None
-        elif len(nodes) == 1:
-            return nodes[0]
-        else:
-            raise Exception('More than one element found for topic name "%s"' % tag_name)
-        
-    def getElementValueByTagName(node, tag_name):
-        node1 = getElementByTagName(node, tag_name)
-        if node1 is None:
-            return None
-        else:
-            value = ''
-            # print '  len(node1.childNodes): %r' % len(node1.childNodes)
-            for child_node in node1.childNodes:
-                # print '  child_node: %r' % child_node
-                if child_node.nodeType == child_node.TEXT_NODE or child_node.nodeType == child_node.CDATA_SECTION_NODE:
-                    value += child_node.nodeValue
-                    
-            return value
-
-
-    tax_term_count = Node.objects.filter(name=tag_name, is_taxonomy_term=True).count()
-
-    """ Different query parameter keys/values that return different result counts.
-    Well, loop thru these in order until we get more than zero results from xplore."""
-    param_options = [
-        {'key': 'thsrsterms', 'value': '"%s"' % tag_name.encode('utf-8')},
-        {'key': 'md', 'value': '"%s"' % tag_name.encode('utf-8')},
-        {'key': 'md', 'value': '%s' % tag_name.encode('utf-8')}
-    ]
-
-    if not tax_term_count:
-        del param_options[0] # no need for thsrsterm so toss out the first item
-
-    for obj in param_options:
-        # clear any previous values
-        if 'thsrsterms' in params:
-            del params['thsrsterms']
-        if 'md' in params:
-            del params['md']
-
-        params[obj['key']] = obj['value']
-
-        url = settings.EXTERNAL_XPLORE_URL + urllib.urlencode(params)
-
-        if settings.DEBUG:
-            log('xplore query: %s' % url)
-            
-        try:
-            file1 = urllib2.urlopen(url)
-        
-            # Get the charset of the request and decode/re-encode the response text into UTF-8 so we can parse it
-            info = file1.info()
-            try:
-                temp, charset = info['content-type'].split('charset=')
-            except ValueError:
-                charset = 'utf-8'
-
-
-        except urllib2.URLError:
-            xplore_error = 'Error: Could not connect to the IEEE Xplore site to download articles.'
-            xplore_results = []
-            totalfound = 0
-        except KeyError:
-            xplore_error = 'Error: Could not determine content type of the IEEE Xplore response.'
-            xplore_results = []
-            totalfound = 0
-
-        else:
-            xplore_error = None
-
-            xml_body = file1.read()
-            file1.close()
-            
-            try:
-                if charset != 'utf-8':
-                    xml_body = xml_body.decode(charset).encode('utf-8')
-
-                xml1 = xml.dom.minidom.parseString(xml_body)
-            except Exception, e:
-                return [], "XML Parse Error: %s" % e, 0
-                
-            try:
-                totalfound = int(getElementValueByTagName(xml1.documentElement, 'totalfound'))
-                
-            # If no records found Xplore will return xml like this and the int parse with raise an exeption
-            # <Error><![CDATA[Cannot go to record 1 since query  only returned 0 records]]></Error>
-            except TypeError:
-                # If there's any query param choice to try, do so.
-                if obj != param_options[-1]:
-                    continue
-                
-                # Otherwise, give up.
-                return [], 'No records found', 0
-                
-            xplore_results = []
-            for document1 in xml1.documentElement.getElementsByTagName('document'):
-                rank = getElementValueByTagName(document1, 'rank')
-                title = getElementValueByTagName(document1, 'title')
-                abstract = getElementValueByTagName(document1, 'abstract')
-                pdf = getElementValueByTagName(document1, 'pdf')
-                authors = getElementValueByTagName(document1, 'authors')
-                pub_title = getElementValueByTagName(document1, 'pubtitle')
-                pub_year = getElementValueByTagName(document1, 'py')
-                
-                # Escape here, since we're going to output this as |safe on the template
-                # title = cgi.escape(title)
-                if highlight_search_term:
-                    title = re.sub('(?i)(%s)' % tag_name, r'<strong>\1</strong>', title)
-                    
-                result = {
-                    'rank': rank,
-                    'name': title,
-                    'description': abstract,
-                    'url': pdf,
-                    'authors': authors,
-                    'pub_title': pub_title,
-                    'pub_year': pub_year,
-                    }
-                
-                xplore_results.append(result)
-
-    return xplore_results, xplore_error, totalfound
 
 @csrf_exempt
 def ajax_recent_xplore(request):
