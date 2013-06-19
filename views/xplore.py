c0f33cde9d467a19b01defd1d1e01caad18a2cc1
coreycantrell <ccantrell@lockify.com>
2013-06-19 11:49:52 -0400

    Stripping html tags from xplore results.

--- views/xplore.py
+++ views/xplore.py
@@ -2,6 +2,7 @@ import cgi
 from django.http import HttpResponse
 from django.utils import simplejson as json
 from logging import debug as log
+from html2text import html2text
 import urllib
 import urllib2
 import re
@@ -182,7 +183,7 @@ def _get_xplore_results(tag_name, highlight_search_term=True, show_all=False, of
             for document1 in xml1.documentElement.getElementsByTagName('document'):
                 rank = getElementValueByTagName(document1, 'rank')
                 title = getElementValueByTagName(document1, 'title')
-                abstract = getElementValueByTagName(document1, 'abstract')
+                abstract = html2text(getElementValueByTagName(document1, 'abstract'))
                 pdf = getElementValueByTagName(document1, 'pdf')
                 authors = getElementValueByTagName(document1, 'authors')
                 pub_title = getElementValueByTagName(document1, 'pubtitle')
