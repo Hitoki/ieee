ef42536cdbd6598d380d3580006525deeb80ce77
2vitalik <2vitalik>
2014-01-29 19:21:58 +0200

    [regroup models]: next is "notification.py"

--- models.py
+++ models.py
@@ -6,32 +6,8 @@ from django.db import models
 from django.db.models.signals import post_save
 
 #from profiler import Profiler
-from new_models.node import Node
-from new_models.resource import Resource, ResourceNodes
-from new_models.society import NodeSocieties
+from new_models.resource import Resource
 from new_models.utils import single_row_or_none, list_to_choices
-
-
-
-class ResourceAdditionNotificationRequest(models.Model):
-    '''Tracks the request of a user to be notified when resources are newly
-    related to a node.'''
-    node = models.ForeignKey(Node, related_name='notification_node')
-    date_created = models.DateTimeField(blank=False, null=False)
-    email = models.CharField(blank=False, max_length=255)
-
-    class Meta:
-        unique_together = ('node', 'email')
-
-
-class ResourceAdditionNotification(models.Model):
-    'Tracke the sending a notification email.'
-    request = models.ForeignKey(ResourceAdditionNotificationRequest)
-    resourceNodes = models.ForeignKey(ResourceNodes, null=True)
-    resourceSocieties = models.ForeignKey(NodeSocieties, null=True)
-    date_notified = models.DateTimeField(blank=False, null=False)
-
-# ----------------------------------------------------------------------------
 
 
 class PermissionManager(models.Manager):
---
+++ new_models/notification.py
@@ -0,0 +1,24 @@
+from django.db import models
+from new_models.node import Node
+from new_models.resource import ResourceNodes
+from new_models.society import NodeSocieties
+
+
+class ResourceAdditionNotificationRequest(models.Model):
+    '''Tracks the request of a user to be notified when resources are newly
+    related to a node.'''
+    node = models.ForeignKey(Node, related_name='notification_node')
+    date_created = models.DateTimeField(blank=False, null=False)
+    email = models.CharField(blank=False, max_length=255)
+
+    class Meta:
+        unique_together = ('node', 'email')
+
+
+class ResourceAdditionNotification(models.Model):
+    'Tracke the sending a notification email.'
+    request = models.ForeignKey(ResourceAdditionNotificationRequest)
+    resourceNodes = models.ForeignKey(ResourceNodes, null=True)
+    resourceSocieties = models.ForeignKey(NodeSocieties, null=True)
+    date_notified = models.DateTimeField(blank=False, null=False)
+
