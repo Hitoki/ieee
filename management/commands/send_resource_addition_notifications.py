0ede3278e114ded4a9cf35af794494acd84f9854
2vitalik <2vitalik>
2014-01-29 13:30:56 +0200

    [pep8]: pep8 for management commands

--- management/commands/send_resource_addition_notifications.py
+++ management/commands/send_resource_addition_notifications.py
@@ -1,28 +1,36 @@
-from django.core.management.base import NoArgsCommand, BaseCommand, CommandError
-from ieeetags.models import Node, ResourceNodes, ResourceAdditionNotificationRequest, ResourceAdditionNotification, NodeSocieties
-from django.core.mail import send_mail, EmailMultiAlternatives
 from datetime import datetime
-from django.template import Context, RequestContext, loader
+
+from django.core.management.base import NoArgsCommand
+from django.core.mail import EmailMultiAlternatives
+from django.template import Context, loader
 from html2text import html2text
-import settings
 from django.contrib.sites.models import Site
 
+from ieeetags.models import ResourceNodes, NodeSocieties, \
+    ResourceAdditionNotificationRequest, ResourceAdditionNotification
+import settings
+
+
 class Command(NoArgsCommand):
-
     def handle_noargs(self, *args, **options):
-
-        emails = ResourceAdditionNotificationRequest.objects.only('email').distinct()
+        emails = ResourceAdditionNotificationRequest.objects. \
+            only('email').distinct()
         for email in emails:
-            reqs = ResourceAdditionNotificationRequest.objects.filter(email=email.email)
+            reqs = ResourceAdditionNotificationRequest.objects. \
+                filter(email=email.email)
             reqs_with_new_items = []
             for req in reqs:
-                previous_notifications = ResourceAdditionNotification.objects.filter(request=req).order_by('-date_notified')
+                previous_notifications = \
+                    ResourceAdditionNotification.objects.filter(request=req). \
+                        order_by('-date_notified')
                 if previous_notifications.count():
                     last_update = previous_notifications[0].date_notified
                 else:
                     last_update = req.date_created
 
-                new_societies = NodeSocieties.objects.filter(node=req.node, date_created__gt=last_update)
+                new_societies = \
+                    NodeSocieties.objects.filter(node=req.node,
+                                                 date_created__gt=last_update)
                 for ns in new_societies:
                     # Save record of this relationship being notified via email
                     nt = ResourceAdditionNotification()
@@ -30,8 +38,10 @@ class Command(NoArgsCommand):
                     nt.resourceSocieties = ns
                     nt.date_notified = datetime.utcnow()
                     nt.save()
-                
-                new_resources = ResourceNodes.objects.filter(node=req.node, date_created__gt=last_update).order_by('resource__resource_type')
+
+                new_resources = ResourceNodes.objects. \
+                    filter(node=req.node, date_created__gt=last_update). \
+                    order_by('resource__resource_type')
                 for nr in new_resources:
                     # Save record of this relationship being notified via email
                     nt = ResourceAdditionNotification()
@@ -55,12 +65,13 @@ class Command(NoArgsCommand):
                     "notification_requests": reqs_with_new_items,
                     "domain": Site.objects.get_current()
                 })
-                body = loader.get_template('email/notify_email.html').render(context)
-        
+                template = loader.get_template('email/notify_email.html')
+                body = template.render(context)
+
                 htmlbody = body
                 body = html2text(body)
-                msg = EmailMultiAlternatives("New resource alert", body , settings.DEFAULT_FROM_EMAIL, [email.email])
+                msg = EmailMultiAlternatives("New resource alert", body,
+                                             settings.DEFAULT_FROM_EMAIL,
+                                             [email.email])
                 msg.attach_alternative(htmlbody, 'text/html')
                 msg.send()
-
-
