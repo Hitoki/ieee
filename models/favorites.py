8de8b5f1daed20483ee048af1c72ada6be646e00
Corey Cantrell <ccantrell@lockify.com>
2014-08-15 12:53:51 -0400

    First pass at resource favorites.

--- models/favorites.py
+++ models/favorites.py
@@ -2,14 +2,21 @@ from django.db import models
 from django.contrib.auth.models import User
 
 from models.node import Node
+from models.resource import Resource
 
 class UserFavorites(models.Model):
 	user = models.OneToOneField(User)
-	favorites = models.ManyToManyField(
+	topics = models.ManyToManyField(
 		Node,
 		blank=True,
 		help_text='Favorite Topics',
-		related_name='user_favorites'
+		related_name='user_favorite_topics'
+	)
+	resources = models.ManyToManyField(
+		Resource,
+		blank=True,
+		help_text='Favorite Resources',
+		related_name='user_favorite_resources'
 	)
 	
 	class Meta:
