77f5bf0b46866104ddb207a96d8fbb97b8aca7f9
vitalik <2vitalik@gmail.com>
2014-08-29 15:16:02 +0300

    pep8 for profiler.py

--- profiler.py
+++ profiler.py
@@ -2,26 +2,27 @@ from logging import debug as log
 import time
 from util import odict
 
+
 class Profiler:
-    '''
+    """
     Used to manually profile functions and see where time is being spent.
-    
+
     Usage:
         p = Profiler('page number 1')
         # ...
         p.tick('Before action X')
-        # ... 
+        # ...
         p.tick('Before action Y')
-        # ... 
+        # ...
         for i in range(10):
             p.start_loop()
-            # ... 
+            # ...
             p.tick('Before action Z')
-            # ... 
+            # ...
             p.end_loop()
-        
+
     Prints all output to the log.
-    '''
+    """
     def __init__(self, name=''):
         self.name = name
         self.is_loop = False
@@ -29,10 +30,11 @@ class Profiler:
         self.start_time = time.time()
         self.loop_start_time = None
         self.last_loop_time = None
-    
+
     def __del__(self):
-        log('profile %s: %0.2fs -- end ----------' % (self.name, time.time() - self.start_time))
-    
+        log('profile %s: %0.2fs -- end ----------' %
+            (self.name, time.time() - self.start_time))
+
     def tick(self, name):
         if self.is_loop:
             if name not in self.ticks:
@@ -42,20 +44,24 @@ class Profiler:
             self.last_loop_time = now
             self.ticks[name] += seconds
         else:
-            log('profile %s: %0.2fs - %s' % (self.name, time.time() - self.start_time, name))
-    
+            log('profile %s: %0.2fs - %s' %
+                (self.name, time.time() - self.start_time, name))
+
     def start_loop(self):
         self.is_loop = True
         self.loop_start_time = time.time()
         self.last_loop_time = self.loop_start_time
         self.ticks = odict()
-        log('profile %s: %0.2fs - START LOOP' % (self.name, time.time() - self.start_time))
-    
+        log('profile %s: %0.2fs - START LOOP' %
+            (self.name, time.time() - self.start_time))
+
     def end_loop(self):
         self.is_loop = False
         self.loop_start_time = None
-        
+
         for name, value in self.ticks.items():
-            log('profile %s: %0.2fs - total for loop tick "%s"' % (self.name, value, name))
-        
-        log('profile %s: %0.2fs - END LOOP' % (self.name, time.time() - self.start_time))
+            log('profile %s: %0.2fs - total for loop tick "%s"' %
+                (self.name, value, name))
+
+        log('profile %s: %0.2fs - END LOOP' %
+            (self.name, time.time() - self.start_time))
