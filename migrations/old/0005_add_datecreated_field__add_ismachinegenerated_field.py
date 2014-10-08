338d0cf5edf2b0f446aa72c7e9c075966fa72a04
vitalik <2vitalik@gmail.com>
2014-10-08 14:44:32 +0300

    moved south migration to the "migrations/old" folder, because we will use django 1.7 migration mechanism

---
+++ migrations/old/0005_add_datecreated_field__add_ismachinegenerated_field.py
@@ -0,0 +1,17 @@
+# encoding: utf-8
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        
+        db.add_column('ieeetags_resource_nodes', 'date_created', models.DateTimeField(blank=True, null=True, default=None))
+        db.add_column('ieeetags_resource_nodes', 'is_machine_generated', models.BooleanField(default=False))
+
+    def backwards(self, orm):
+        db.delete_column('ieeetags_resource_nodes', 'date_created')
+        db.delete_column('ieeetags_resource_nodes', 'is_machine_generated')
+
